e294bedb18df0bd795bb471fe43c305caed6f0a8
vvvvvv8818 <vvvvvv8818@gmail.com>
2018-09-09 20:40:39 +0900

    workout + recog

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var spawn	= require('child_process').spawn;
 var cons	= require('consolidate');
 var path	= require('path');
 var fs		= require('fs');
+var request = require('request')
 var externals = require('./externals.js');
 
 app.engine('ejs', cons.ejs);
@@ -18,13 +19,13 @@ app.use(express.static('public'));
 
 
 var MYIP = externals.MYIP;
-//var PORT = externals.MYPORT;
-var PORT = 8188;
+var PORT = externals.PORT;
 var MYADDR = externals.MYADDR;
+var FITBEE_PATH = externals.FITBEE_PATH;
 var awsServer = 'http://13.124.65.48:3000';
 
 var mycss = {
-    style: fs.readFileSync('./stylesheet/background.css', 'utf8')
+    style: fs.readFileSync( './stylesheet/background.css', 'utf8')
 };
 var mycss2 = {
     style: fs.readFileSync('./stylesheet/video_style.css', 'utf8')
@@ -38,7 +39,7 @@ var USER_INFO = '';
 app.get('/login', function(req, res){
 	console.log('login');
 	var child = spawn('python', ['./faceDetect.py']);
-
+	console.log('exec python');
 	child.stdout.on('data',function(chunk){
     	var textChunk = chunk.toString('utf8');
 		console.log('textChunk : ' + textChunk);
@@ -54,7 +55,7 @@ app.get('/login', function(req, res){
 			
 			request.post({url:awsServer+'/user', formData: formData}, function optionalCallback(err, httpResponse, body) {
 				if (err) { return console.error('upload failed:', err); }
-				USER_INFO = body;
+				USER_INFO = JSON.parse(body);
 				res.send(body);
 			});
 		}
@@ -62,6 +63,11 @@ app.get('/login', function(req, res){
 			res.send('-1');
 		}
 	});
+/*	res.render('start',{
+		mycss: mycss
+		}
+	);
+*/
 });
 
 
@@ -79,11 +85,25 @@ app.get('/main', function(req, res){
 
 	});
 
-		mycss: mycss, name:USER_INFO.userName
-	});
+	res.render('main.ejs', {
+		mycss: mycss, 
+		userName: USER_INFO.userName,
+		weak_part: USER_INFO.weak_part,
+		concent_part: USER_INFO.concent_part,
+		goal_weight: USER_INFO.goal_weight,
+		goal_muscle: USER_INFO.goal_muscle,
+		goal_fat: USER_INFO.goal_fat,
+		init_weight: USER_INFO.init_weight,
+		init_muscle: USER_INFO.init_muscle,
+		init_fat: USER_INFO.init_fat,
+		now_weight: USER_INFO.now_weight,
+		now_muscle: USER_INFO.now_muscle,
+		now_fat: USER_INFO.now_fat 
+		}
+	);
+
 	//res.sendFile(__dirname+'/main.html');
 });
-
 
 
 app.get('/routine', function(req, res){
@@ -115,17 +135,28 @@ app.get('/routine', function(req, res){
 	console.log(MYIP);
 	console.log(PORT);
 	console.log(MYADDR);
-	res.render('routine', {result:result});
+/*	res.render('routine', {
+		result:result
+		mycss3: mycss3
+	});
+*/
 });
-
 
 
 
 app.get('/workout', function(req, res){
 	// 민정 프로세스 키면 횟수 세질때 마다 데이터 날아옴
+	io.on('connection', function(socket2){
+		console.log('start workout');
+
+		var child = spawn('python', ['./workout.py']);
+		setting_workout(child, socket2);
+
+		var child_recog = spawn('node', ['./child_recog.js']);
+		setting_recog(socket2, child_recog);
+	});
 	res.sendFile(__dirname + '/workout.html');
 });
-//루틴안내 html보여주기
 
 
 app.get('/takepic', function(req, res){
@@ -137,20 +168,45 @@ app.get('/takepic', function(req, res){
 		setting_child(child, socket);
 	});
 
-	res.render('takebosypic.ejs',{
+	res.render('takebodypic.ejs',{
 		mycss2: mycss2
 	});
 });
 
-
-app.get('/mydata', function(req, res){
+/*
+app.get('/data', function(req, res){
 	result = 'test result'
+
 	var options = {
 		host: '13.124.65.48',
 		port: 3000,
-		path: '/user/inbody/1'
+		path: '/user/inbody/1'//+USER_INFO.userId
 	}
+	var INBODY_DATA;
+	var CH_DATA;
+			
+	var formData = ''
+	request.post(
+		{url:awsServer+'/user/inbody/1', formData: formData}, 
+		function optionalCallback(err, httpResponse, data) {
+				if (err) { return console.error('upload failed:', err); }
+				INBODY_DATA = JSON.parse(data).inbody_data;
+				console.log(INBODY_DATA);
+				CH_DATA = JSON.parse(data).change_data;
+				//res.send(body);
+		});
 
+	res.render('data', {
+			mycss3:mycss3,
+			weight: INBODY_DATA.weight,
+			muscle: INBODY_DATA.muscle,
+			fat_percent: INBODY_DATA.fat_percent,
+			bmi: INBODY_DATA.bmi
+	});
+	
+*/
+
+/*
 	server.on('request', (req, response) => {
 		var awsReq = http.get(options, (awsRes) => {
 			awsRes.setEncoding('utf-8');
@@ -159,19 +215,61 @@ app.get('/mydata', function(req, res){
 			awsRes
 			.on('data', (chunk) => {
 				result += chunk;
-				console.log('[/mydata] chunk : ' + chunk);
+				console.log('[/data] chunk : ' + chunk);
 			})
 			.on('end', () => {
-				console.log('[/mydata] result : ' + result);
-				response.render('mydata.ejs', {
-					mycss3: mycss3
+				console.log('[/data] result : ' + result);
+				INBODY_DATA = JSON.parse(result).inbody_data;
+				CH_DATA = JSON.parse(result).change_data;
+
+				response.render('data', {
+					mycss3: mycss3,
+					weight: INBODY_DATA.weight,
+					muscle: INBODY_DATA.muscle,
+					fat_percent: INBODY_DATA.fat_percent,
+					bmi: INBODY_DATA.bmi
 				});
 			});
 		});
 	});	//server.on
+*/
 
+/*
+	result = request_AWS('/user/inbody/1');
+	console.log('### ' + result);
+*/
 
-});
+//});
+
+function request_AWS(url){
+	var options = {
+		host: '13.124.65.48',
+		port: 3000,
+		path: url
+	}
+	var INBODY_DATA;
+	var CH_DATA;
+
+	server.on('request', (req, response) => {
+		var awsReq = http.get(options, (awsRes) => {
+			awsRes.setEncoding('utf-8');
+
+			result = '';
+			
+			awsRes
+			.on('data', (chunk) => {
+				result += chunk;
+				console.log('[/data] chunk : ' + chunk);
+			})
+			.on('end', () => {
+				console.log('[/data] result : ' + result);
+				result = JSON.parse(result).inbody_data;
+				return result;
+			});
+		});
+	});
+}
+
 
 
 function setting_recog(socket, child){
@@ -186,6 +284,7 @@ function setting_recog(socket, child){
 	});
 
 
+
 	console.log('[setting_child]');
 	child.stdout.on('data', function(data){
 		console.log('from child : ' + data);
@@ -207,39 +306,21 @@ function setting_recog(socket, child){
 
 
 
-/*
-function setting_socket(socket){
-	console.log('[setting_socket]');
-	socket.on('main', function(msg){
-		console.log('msg : ' + msg);
+
+function setting_workout(child, socket2){
+	console.log('[workout_child]');
+	child.stdout.on('data',function(count){
+		console.log('count : '+ count);
+		socket2.emit('workout', count.toString().trim());
 	});
 
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
-}
-*/
-/*
-function setting_child(child, socket){
-	console.log('[setting_child]');
-	child.stdout.on('data', function(data){
-		console.log('from child : ' + data);
-		//console.dir(data);
-
-		if (data == 'WORKOUT\n' || data == 'TAKEPIC\n' || data == 'MYDATA\n'){	// Object ArrayBuffer
-			console.log('send ' + data.toString().trim() + ' to main.html');
-			socket.emit('main', data.toString().trim());
-		}
-	});
-
-	child.stderr.on('data', function(data){
-		process.stdout.write(data);
-	});
-
-	child.on('exit', function(code) {
-		console.log('Child exited with code :' + code);
+	child.stderr.on('data',function(count){
+		console.log('test'+count);
 	});
 }
-*/
 
-server.listen(PORT, function(){ console.log('listening on ' + PORT); });
+
+server.listen(PORT, function(){ 
+	console.log(FITBEE_PATH);
+	console.log('listening on ' + PORT); 
+});
--- externals.js
+++ externals.js
@@ -1,5 +1,6 @@
 /* 외부 변수/함수/객체 선언하는 곳 */
 
-exports.PORT = 8188;
+exports.PORT = 1313;
 exports.MYIP = '192.168.0.102';
-exports.MYADDR = '192.168.0.102:8188';
+exports.MYADDR = '192.168.0.102:' + this.PORT;
+exports.FITBEE_PATH = '/home/pi/piserver';
